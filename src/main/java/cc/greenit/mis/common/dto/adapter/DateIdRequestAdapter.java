34a0f536a6cdf9de84edb630fb213e3f70655a94
GreenIT-Neal <skgt5565@greenit.cc>
2025-03-25 15:27:32 +0900

    first

---
+++ src/main/java/cc/greenit/mis/common/dto/adapter/DateIdRequestAdapter.java
@@ -0,0 +1,8 @@
+package cc.greenit.mis.common.dto.adapter;
+
+public interface DateIdRequestAdapter {
+    String getDateId();
+    String getDateIdLike();
+    String getDateIdBetweenFrom();
+    String getDateIdBetweenTo();
+}
