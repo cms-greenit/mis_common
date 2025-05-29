521668df52880d6e157fbd7bce6a70a58543df5c
GreenIT-Neal <skgt5565@greenit.cc>
2025-05-29 14:25:40 +0900

    [ADD] : addPlayYn / 추가홀 여부 추가

--- src/main/java/cc/greenit/mis/common/dto/booking/GetTeeTimeListResponseDTO.java
+++ src/main/java/cc/greenit/mis/common/dto/booking/GetTeeTimeListResponseDTO.java
@@ -9,6 +9,7 @@ public class GetTeeTimeListResponseDTO {
     private String timeId;
     private String timeSequence;
     private Integer part;
+    private String addPlayYn;
     private Integer hole;
     private Integer minimumHeadcount;
     // CADDY("캐디동반"), SELF("셀프라운드"), DRIVING("드라이빙캐디"), MARSHALL("마샬캐디"), INTERN("인턴캐디"), SMILE("미소지기캐디"), SPROUT("새싹캐디");
