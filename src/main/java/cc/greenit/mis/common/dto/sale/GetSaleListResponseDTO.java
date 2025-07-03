73f510ca14142dbb032aac0f3f132d51ec0b541c
GreenIT-Neal <skgt5565@greenit.cc>
2025-07-03 12:14:42 +0900

    [ADD] : SaleList - SaleType 추가

--- src/main/java/cc/greenit/mis/common/dto/sale/GetSaleListResponseDTO.java
+++ src/main/java/cc/greenit/mis/common/dto/sale/GetSaleListResponseDTO.java
@@ -6,6 +6,7 @@ import java.math.BigDecimal;
 
 @Data
 public class GetSaleListResponseDTO {
+    private String saleType;
     private String dateId;
     private String courseId;
     private String timeId;
