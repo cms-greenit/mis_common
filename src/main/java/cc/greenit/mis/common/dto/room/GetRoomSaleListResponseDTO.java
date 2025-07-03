55d63c74058e7c38bf7696644e019f96965b0342
GreenIT-Neal <skgt5565@greenit.cc>
2025-07-03 09:04:37 +0900

    [CHG] : room-sale 수정

--- src/main/java/cc/greenit/mis/common/dto/room/GetRoomSaleListResponseDTO.java
+++ src/main/java/cc/greenit/mis/common/dto/room/GetRoomSaleListResponseDTO.java
@@ -6,18 +6,11 @@ import lombok.Data;
 public class GetRoomSaleListResponseDTO {
     private String companyId;
     private String bookingId;
-    private String bookingSequence;
-    private String bookerName;
-    private String roomCode;
-    private String roomName;
-    private String roomCount;
-    private String visitorCount;
-    private String guestName;
-    private String guestMembershipNumber;
-    private Integer stayNight;
-    private String roomChargeCode;
-    private String roomChargeAmount;
-    private String checkInDate;
-    private String checkOutDate;
-    private String reservedAt;
+    private String checkInSequence;
+    private String sequenceNumber;
+    private String dateId;
+    private String shopId;
+    private String roomNumber;
+    private String supplyAmount;
+    private String totalAmount;
 }
