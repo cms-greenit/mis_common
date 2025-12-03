0837b645cc001c33624ed626e5474d8c75538a99
eddy <qwe7662@naver.com>
2025-12-03 11:08:42 +0900

    [ADD] MisApiEndpoints 정의 추가

---
+++ src/main/java/cc/greenit/mis/common/config/MisApiEndpoints.java
@@ -0,0 +1,17 @@
+package cc.greenit.mis.common.config;
+
+public class MisApiEndpoints {
+
+    private final static String CALENDAR = "/master/calendar";
+    private final static String MEMBER = "/master/member-list";
+    private final static String RENTAL = "/master/rental-list";
+    private final static String BOOKING = "/booking/time-list";
+    private final static String VISIT = "/visit/checkin-list";
+    private final static String SALE = "/sale/sale-list";
+
+    private final static String SIMPLE_RENTAL = "/master/simple/rental-list";
+    private final static String SIMPLE_BOOKING = "/booking/simple/time-list";
+    private final static String SIMPLE_VISIT = "/visit/simple/checkin-list";
+    private final static String SIMPLE_SALE = "/sale/simple/sale-list";
+}
+
